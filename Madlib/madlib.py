dc57fa979a5ebe999d4f83eca6935af700ce7572
Sreeram Shenai <sreeramshenai2000@gmail.com>
2022-05-19 00:46:31 +0530

    madlib

---
+++ Madlib/madlib.py
@@ -0,0 +1,8 @@
+def madlib():
+    language = input('Programming Language: ')
+    verb = input('Verb: ')
+    hero = input('Hero: ')
+    print(f'{language} is an amazing programming language.It is very easy to learn and {verb} and has a very supportive community.Lets go ahead and code like {hero}.')
+
+if __name__ == '__main__':
+    madlib()
